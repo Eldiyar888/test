c98e7568e716dfd8a06227b9092606e35f68fedd
Eldiyar888 <jashtilekov888@gmail.com>
2023-02-12 15:41:48 +0600

    add function size

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function sum(a, b) {
 
 const res = sum(1, 2)
 console.log(res);
+
+
+function sizeOfStr(str) {
+    return str.length
+}
+
+const res2 = sizeOfStr('privet kak dela!')
+console.log(res2)
